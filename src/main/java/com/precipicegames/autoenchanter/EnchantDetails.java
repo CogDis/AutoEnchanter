93d2d9b72b6151070199a0b78d27bcaf337415b6
Elias Woods <EliasWoods@gmail.com>
2011-12-09 12:09:08 -0500

    Add new EnchantClass

---
+++ src/main/java/com/precipicegames/autoenchanter/EnchantDetails.java
@@ -0,0 +1,11 @@
+package com.precipicegames.autoenchanter;
+
+import java.util.HashMap;
+
+import org.bukkit.enchantments.Enchantment;
+
+public class EnchantDetails extends HashMap<Enchantment, Double> {
+
+	private static final long serialVersionUID = 7089834690782156125L;
+	
+}
